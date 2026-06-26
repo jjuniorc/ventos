79f025ff0b1cf9b4a1b7bdddfe4c790e7fc3dac8
João Costa <jjuniorc@gmail.com>
2026-06-25 23:54:53 -0300

    JPA and Hibernate configuration

--- src/main/java/com/vacavoa/ventos/service/VentosMessageService.java
+++ src/main/java/com/vacavoa/ventos/service/VentosMessageService.java
@@ -1,11 +1,15 @@
 package com.vacavoa.ventos.service;
 
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vacavoa.ventos.repository.VentosMessageRepository;
 
 @Service
 public class VentosMessageService {
+    // @Autowired
+    // private VentosMessageRepository messageRepository;
+    
     private final VentosMessageRepository messageRepository;
     
     public VentosMessageService(VentosMessageRepository messageRepository)
