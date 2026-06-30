6057579d08ba843f1ef10926a10d3fce4034e390
João Costa <jjuniorc@gmail.com>
2026-06-30 00:54:57 -0300

    Security and Authentication

--- src/main/java/com/vacavoa/ventos/controller/AuthController.java
+++ src/main/java/com/vacavoa/ventos/controller/AuthController.java
@@ -3,8 +3,47 @@ package com.vacavoa.ventos.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vacavoa.ventos.model.User;
+import com.vacavoa.ventos.security.JwtUtil;
+import com.vacavoa.ventos.service.UserService;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request) 
+    {
+        User user = userService.registerUser(request.get("username"), request.get("password"));
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) 
+    {
+        Optional<User> user = userService.findByUsername(request.get("username"));
+
+        if(user.isPresent() && user.get().getPassword().equals(request.get("password")))
+        {
+            String token = new JwtUtil().generateToken(user.get().getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+
+        return ResponseEntity.status(401).body("Invalid Credentials");
+    }  
     
 }
